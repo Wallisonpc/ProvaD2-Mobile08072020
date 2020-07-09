3bd3b293e6ec46ead666ec0671dfa4b8b45137dc
wallison <wallison.pc@gmail.com>
2020-07-08 21:42:12 -0300

    committ

---
+++ ProvaD2-Mobile/src/app/professor/professor.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ProfessorComponent } from './professor.component';
+
+describe('ProfessorComponent', () => {
+  let component: ProfessorComponent;
+  let fixture: ComponentFixture<ProfessorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProfessorComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ProfessorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ProvaD2-Mobile/src/app/services/ProfessorService.service.ts
@@ -0,0 +1,16 @@
+import { professor } from '../models/professor.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+  export class ProfessorService{
+      constructor (){}
+  
+  public getProfessor(professor: professor){
+   
+
+  }
+  
+    }
